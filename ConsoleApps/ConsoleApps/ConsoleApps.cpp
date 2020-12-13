e7a11af675c72c5c265398f38499ae37cb28c3e5
Aaron Lemmon <lemmo031@morris.umn.edu>
2020-12-13 14:54:56 -0800

    Added basic console application.

---
+++ ConsoleApps/ConsoleApps/ConsoleApps.cpp
@@ -0,0 +1,12 @@
+﻿// ConsoleApps.cpp : Defines the entry point for the application.
+//
+
+#include "ConsoleApps.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
