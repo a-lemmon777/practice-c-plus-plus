7c58823f8cb893dda0682a110cf1fc5d45df5cdd
Aaron Lemmon <lemmo031@morris.umn.edu>
2020-12-13 15:29:05 -0800

    Made a single-node neural network.

--- ConsoleApps/ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps/ConsoleApps.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	double inputs[] { 1.2, 5.1, 2.1 };
+	double weights[] { 3.1, 2.1, 8.7 };
+	double bias = 3.0;
+	double output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias;
+
+	cout << "Output: " << output << endl;
 	return 0;
 }
