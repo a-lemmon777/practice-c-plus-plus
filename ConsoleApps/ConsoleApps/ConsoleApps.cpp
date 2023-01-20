b3675743b758e4080e75a585c244126dcdf11936
Aaron Lemmon <lemmo031@morris.umn.edu>
2023-01-20 12:48:43 -0600

    Added a local variable to my Hello World program.

--- ConsoleApps/ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps/ConsoleApps.cpp
@@ -1,7 +1,10 @@
-﻿#include <iostream>
+﻿// Hello World Program
+#include <iostream>
 
 int main()
 {
+	int hours = 5;
 	std::cout << "Yay, I'm learning C++!" << std::endl;
+	std::cout << hours << std::endl;
 	return 0;
 }
