f8bd18fd504e05bf542d769afc2b0e883cbd1b23
Aaron Lemmon <lemmo031@morris.umn.edu>
2023-01-21 15:57:05 -0600

    Altered program to be a calorie counter.

--- ConsoleApps/ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps/ConsoleApps.cpp
@@ -1,10 +1,18 @@
-﻿// Hello World Program
+﻿// Calorie Counter Program
 #include <iostream>
 
 int main()
 {
-	int hours = 5;
+	int carb_grams;
+	int fat_grams;
+	int protein_grams;
+
+	carb_grams = 10;
+	fat_grams = 20;
+	protein_grams = 15;
+
+	int total_calories = carb_grams * 4 + fat_grams * 9 + protein_grams * 4;
 	std::cout << "Yay, I'm learning C++!" << std::endl;
-	std::cout << hours << std::endl;
+	std::cout << "There are " << total_calories << " total calories." << std::endl;
 	return 0;
 }
