d6258162bab1edae75a263ae92c69fd284a92431
Aaron Lemmon <lemmo031@morris.umn.edu>
2023-01-23 13:32:16 -0600

    Added a weeks-to-days calculator to ConsoleApps.cpp.

--- ConsoleApps/ConsoleApps/ConsoleApps.cpp
+++ ConsoleApps/ConsoleApps/ConsoleApps.cpp
@@ -10,5 +10,10 @@ int main()
 	int total_calories = carb_grams * 4 + fat_grams * 9 + protein_grams * 4;
 	std::cout << "Yay, I'm learning C++!" << std::endl;
 	std::cout << "There are " << total_calories << " total calories." << std::endl;
+
+	// Weeks to days calculation
+	int weeks = 20;
+	int days = weeks * 7;
+	std::cout << "There are " << days << " days in " << weeks << " week(s)." << std::endl;
 	return 0;
 }
