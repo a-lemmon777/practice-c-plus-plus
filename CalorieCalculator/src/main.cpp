e549da85b50c267b591dd063bc6e70a81e9d30ae
Aaron Lemmon <lemmo031@morris.umn.edu>
2023-01-24 21:21:04 -0600

    Readded main CalorieCalculator file.

---
+++ CalorieCalculator/src/main.cpp
@@ -0,0 +1,19 @@
+﻿// Calorie Counter Program
+#include <iostream>
+
+int main()
+{
+	int carb_grams = 10;
+	int fat_grams = 20;
+	int protein_grams = 15;
+
+	int total_calories = carb_grams * 4 + fat_grams * 9 + protein_grams * 4;
+	std::cout << "Yay, I'm learning C++!" << std::endl;
+	std::cout << "There are " << total_calories << " total calories." << std::endl;
+
+	// Weeks to days calculation
+	int weeks = 20;
+	int days = weeks * 7;
+	std::cout << "There are " << days << " days in " << weeks << " week(s)." << std::endl;
+	return 0;
+}
