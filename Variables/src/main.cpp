004d4ca4282634f3ab5c3319eddfde04176a7e79
Aaron Lemmon <lemmo031@morris.umn.edu>
2023-01-27 15:51:37 -0600

    Retitled Variables project.

--- Variables/src/main.cpp
+++ Variables/src/main.cpp
@@ -1,14 +1,14 @@
-﻿// Calorie Counter Program
+﻿// Program with variables
 #include <iostream>
 
 int main()
 {
+	// Calorie calculator
 	int carb_grams = 10;
 	int fat_grams = 20;
 	int protein_grams = 15;
 
 	int total_calories = carb_grams * 4 + fat_grams * 9 + protein_grams * 4;
-	std::cout << "Yay, I'm learning C++!" << std::endl;
 	std::cout << "There are " << total_calories << " total calories." << std::endl;
 
 	// Weeks to days calculation
