6d104c39a8090ef4083c13782895beabf66c5301
Aaron Lemmon <lemmo031@morris.umn.edu>
2023-01-27 16:26:20 -0600

    Changed tabs to spaces.

--- 001Variables/src/main.cpp
+++ 001Variables/src/main.cpp
@@ -3,17 +3,17 @@
 
 int main()
 {
-	// Calorie calculator
-	int carb_grams = 10;
-	int fat_grams = 20;
-	int protein_grams = 15;
+    // Calorie calculator
+    int carb_grams = 10;
+    int fat_grams = 20;
+    int protein_grams = 15;
 
-	int total_calories = carb_grams * 4 + fat_grams * 9 + protein_grams * 4;
-	std::cout << "There are " << total_calories << " total calories." << std::endl;
+    int total_calories = carb_grams * 4 + fat_grams * 9 + protein_grams * 4;
+    std::cout << "There are " << total_calories << " total calories." << std::endl;
 
-	// Weeks to days calculation
-	int weeks = 20;
-	int days = weeks * 7;
-	std::cout << "There are " << days << " days in " << weeks << " week(s)." << std::endl;
-	return 0;
+    // Weeks to days calculation
+    int weeks = 20;
+    int days = weeks * 7;
+    std::cout << "There are " << days << " days in " << weeks << " week(s)." << std::endl;
+    return 0;
 }
