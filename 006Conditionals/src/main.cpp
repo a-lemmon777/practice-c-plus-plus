603adc9c4144a2d46cb336ae9227f62df3b2f72c
Aaron Lemmon <a.lemmon777@gmail.com>
2023-02-04 12:03:16 -0600

    Added the conditional logic in the Conditionals project.

--- 006Conditionals/src/main.cpp
+++ 006Conditionals/src/main.cpp
@@ -23,37 +23,89 @@ int main()
         vehicle_choice = 5; // You're in a yacht.
     }
 
-    /*
-    * Negative
-    *   Submarine: Enjoy the depths of the deep blue sea!
-    *   Not in vehicle: Don't gawk at the eels, they're shy.
-    *   Other: You've sunk your ride.
-    * 
-    * 0
-    *   Submarine: Feels good to surface for some fresh air.
-    *   Yacht: Cruising through the waves.
-    *   Not in vehicle: It's a perfect day for a swim!
-    *   Other: Oops, an unexpected detour into the sea!
-    * 
-    * 1 - 5,000 ft
-    *   Submarine and Yacht: You've run aground!
-    *   Car: Time to blast the road trip playlist!
-    *   Airplane: Please prepare for takeoff.
-    *   Not in vehicle: Just loitering around on solid ground I guess.
-    * 
-    * 5,000 - 30,000 ft
-    *   Submarine and Yacht: You must have hitched a ride on a big tsunami to get on this mountain.
-    *   Car: It's all downhill from here!
-    *   Not in vehicle: You're an impressive mountaineer!
-    *   Airplane: Watch out for mountains! Swerve around them if you have to.
-    * 
-    * > 30,000 ft
-    *   Not in a vehicle: I hope you have a parachute!
-    *   Airplane: Nice, cruising altitude. Bust out the snacks.
-    *   Other: I'm pretty impressed you managed to get your vehicle up this high.
-    */
-
     std::cout << "Altitude: " << altitude << std::endl;
-    std::cout << "Vehicle choice: " << vehicle_choice << std::endl;
+    std::cout << "Vehicle choice: ";
+    if (vehicle_choice == 1) {
+        std::cout << "car";
+    }
+    else if (vehicle_choice == 2) {
+        std::cout << "submarine";
+    }
+    else if (vehicle_choice == 3) {
+        std::cout << "airplane";
+    }
+    else if (vehicle_choice == 4) {
+        std::cout << "no vehicle";
+    }
+    else {
+        std::cout << "yacht";
+    }
+    std::cout << std::endl;
+
+    if (altitude < 0) {
+        if (vehicle_choice == 2) { // submarine
+            std::cout << "Enjoy the depths of the deep blue sea!" << std::endl;
+        }
+        else if (vehicle_choice == 4) { // no vehicle
+            std::cout << "Don't gawk at the eels, they're shy." << std::endl;
+        }
+        else {
+            std::cout << "You've sunk your ride." << std::endl;
+        }
+    }
+    else if (altitude == 0) {
+        if (vehicle_choice == 2) { // submarine
+            std::cout << "Feels good to surface for some fresh air." << std::endl;
+        }
+        else if (vehicle_choice == 4) { // no vehicle
+            std::cout << "It's a perfect day for a swim!" << std::endl;
+        }
+        else if (vehicle_choice == 5) { // yacht
+            std::cout << "Cruising through the waves." << std::endl;
+        }
+        else {
+            std::cout << "Oops, an unexpected detour into the sea!" << std::endl;
+        }
+    }
+    else if (altitude <= 5000) {
+        if (vehicle_choice == 1) { // car
+            std::cout << "Time to blast the road trip playlist!" << std::endl;
+        }
+        else if (vehicle_choice == 2 || vehicle_choice == 5) { // submarine or yacht
+            std::cout << "You've run aground!" << std::endl;
+        }
+        else if (vehicle_choice == 3) { // airplane
+            std::cout << "Please prepare for takeoff." << std::endl;
+        }
+        else { // no vehicle is the only possibility left
+            std::cout << "Just loitering around on solid ground I guess." << std::endl;
+        }
+    }
+    else if (altitude <= 30000) {
+        if (vehicle_choice == 1) { // car
+            std::cout << "It's all downhill from here!" << std::endl;
+        }
+        else if (vehicle_choice == 2 || vehicle_choice == 5) { // submarine or yacht
+            std::cout << "You must have hitched a ride on a big tsunami to get on this mountain." << std::endl;
+        }
+        else if (vehicle_choice == 3) { // airplane
+            std::cout << "Watch out for mountains! Swerve around them if you have to." << std::endl;
+        }
+        else { // no vehicle is the only possibility left
+            std::cout << "You're an impressive mountaineer!" << std::endl;
+        }
+    }
+    else {
+        if (vehicle_choice == 3) { // airplane
+            std::cout << "Nice, cruising altitude. Bust out the snacks." << std::endl;
+        }
+        else if (vehicle_choice == 4) { // no vehicle
+            std::cout << "I hope you have a parachute!" << std::endl;
+        }
+        else {
+            std::cout << "I'm pretty impressed you managed to get your vehicle up this high." << std::endl;
+        }
+    }
+
     return 0;
 }
