0c467fbb79a8cc510c8660cc5c797c320314877f
Aaron Lemmon <a.lemmon777@gmail.com>
2023-03-24 12:12:49 -0500

    Improved formatting of 011RandomNumbers.

--- 011RandomNumbers/src/main.cpp
+++ 011RandomNumbers/src/main.cpp
@@ -7,30 +7,27 @@ int main()
 {
     // Generate a random number between 1 and 100 inclusive.
     std::srand(std::time(0));
-    int secretNumber = (std::rand() % 100) + 1;
+    const int secretNumber = (std::rand() % 100) + 1;
 
     // Ask the player for a guess.
-    int playerGuess;
+    int playerGuess = 0;
     int guessCount = 0;
-    std::cout << "Please enter a number between 1 and 100 inclusive." << std::endl;
-    std::cin >> playerGuess;
-    guessCount++;
 
     // Keep looping until the player guesses correctly.
-    while (playerGuess != secretNumber) {
-        // Tell the player if they guessed too high or too low.
-        if (playerGuess < secretNumber) {
-            std::cout << "Your guess was too low. Try again!" << std::endl;
-        }
-        else if (playerGuess > secretNumber) {
-            std::cout << "Your guess was too high. Try again!" << std::endl;
-        }
-
-        // Ask the player for another guess
+    do
+    {
+        // Ask the player for a guess
         std::cout << "Please enter a number between 1 and 100 inclusive." << std::endl;
         std::cin >> playerGuess;
         guessCount++;
-    }
+
+        // Tell the player if they guessed too high or too low.
+        if (playerGuess < secretNumber)
+            std::cout << "Your guess was too low. Try again!" << std::endl;
+        else if (playerGuess > secretNumber)
+            std::cout << "Your guess was too high. Try again!" << std::endl;
+
+    } while (playerGuess != secretNumber);
 
     // Congratulate the player and tell them how many guesses it took.
     std::cout << "Congratulations, you guessed right! It took you " << guessCount << " guess(es).";
