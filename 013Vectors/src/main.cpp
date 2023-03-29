d817967ea8a4ec9c46751f07914738c03d0c6b05
Aaron Lemmon <a.lemmon777@gmail.com>
2023-03-29 12:24:48 -0500

    Added additional vector usage scenarios to 013Vectors.

--- 013Vectors/src/main.cpp
+++ 013Vectors/src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> vectorOutsideOfFunction(4);
+
 int GetFive()
 {
     return 5;
@@ -20,7 +22,7 @@ void PrintVectorInfo(const std::vector<int>& vectorToPrint)
 
 int main()
 {
-    // Using pushback
+    // Using pushback.
     std::vector<int> myVector;
     myVector.push_back(5);
     myVector.push_back(6);
@@ -29,80 +31,79 @@ int main()
     std::cout << "Added 3 items to myVector." << std::endl;
     PrintVectorInfo(myVector);
 
-    // Resizing smaller
+    // Resizing smaller.
     myVector.resize(2);
     std::cout << "Resized myVector to size 2." << std::endl;
     PrintVectorInfo(myVector);
-    
-    // resizing larger
-    // resizing larger with a specified value for the new elements.
-    // resizing smaller
 
-    // making a vector with an initial size
-    // maybe initializing vectors inside functions vs outside functions.
+    // Resizing larger.
+    myVector.resize(4);
+    std::cout << "Resized myVector to size 4." << std::endl;
+    PrintVectorInfo(myVector);
+
+    // Resizing larger with a specified value for the new elements.
+    myVector.resize(6, 8);
+    std::cout << "Resized myVector to size 6, with a value of 8 being specified for the new elements." << std::endl;
+    PrintVectorInfo(myVector);
+
+    // Making a vector with an initial size.
+    std::vector<int> vectorWithInitialSize(3);
+    std::cout << "Declared a vector with an initial size of 3." << std::endl;
+    PrintVectorInfo(vectorWithInitialSize);
+
+    // Inspecting a vector declared outside of a function.
+    std::cout << "Declared a vector with initial size of 4 outside a function." << std::endl;
+    PrintVectorInfo(vectorOutsideOfFunction);
+
     // Initializing a vector with brace notation.
-    // Initializing a vector with brace notation and a matching size.
-    // Initializing a vector with brace notation and smaller size.
-    // Initializing a vector with brace notation and a bigger size.
+    std::vector<int> vectorWithBraceNotation = { 1, 2, 3 };
+    std::cout << "Declared a vector initialized with brace notation." << std::endl;
+    PrintVectorInfo(vectorWithBraceNotation);
+
+    // Initializing a vector with brace notation and a matching size. This is not valid.
+    //std::vector<int> vectorWithBraceNotationAndMatchingSize(2){ 4, 5 };
+
     // Initializing a vector with all the same value.
+    std::vector<int> vectorFilledWithSingleValue(5, 9);
+    std::cout << "Declared a vector with size 5, with all values set to 9." << std::endl;
+    PrintVectorInfo(vectorFilledWithSingleValue);
+
     // Initializing with a size that's determined at runtime.
+    std::vector<int> vectorWithSizeDeterminedAtRuntime(GetFive());
+    std::cout << "Declared a vector whose size is determined at runtime. You couldn't do this with an array."
+        << std::endl;
+    PrintVectorInfo(vectorWithSizeDeterminedAtRuntime);
 
     // Assigning one vector to another
+    std::vector<int> vector1 = { 10, 11, 12 };
+    std::vector<int> vector2 = vector1;
+    std::cout << "Declared a vector using the assignment operator with another vector." << std::endl;
+    PrintVectorInfo(vector2);
+
     // "Overwriting" a vector with another one
-    // Assigning a vector to another, then modifying to see if it changes both.
-    // Experimenting with const on a vector
-    // accessing out of bounds with both square brackets and at()
+    std::vector<int> vector3 = { 13, 14, 15 };
+    std::vector<int> vector4 = { 1, 2, 3 };
+    vector4 = vector3;
+    std::cout << "Overwrote a vector with another vector using the assignment operator." << std::endl;
+    PrintVectorInfo(vector4);
 
+    // Assigning a vector to another, then modifying the first to see if it changes both.
+    std::vector<int> vector5 = { 16, 17, 18 };
+    std::vector<int> vector6 = vector5;
+    vector5[0] = 0;
+    std::cout << "Assigned one vector to another, then modified the first to see if both variables reference the "
+        "same vector in memory. They are indeed separate copies." << std::endl;
+    PrintVectorInfo(vector6);
 
+    // Experimenting with const on a vector. Modifying the elements of the vector is not allowed by the compiler.
+    const std::vector<int> constVector = { 19, 20, 21 };
+    //constVector[0] = 0;
+    //constVector.push_back(22);
 
-    std::vector<int> otherVector = myVector;
-
-    std::cout << myVector[0] << std::endl;
-    std::cout << otherVector[0] << std::endl;
-    
-    myVector[0] = 8;
-
-    std::cout << myVector[0] << std::endl;
-    std::cout << otherVector[0] << std::endl;
-
-    std::vector<float> floatVector(3);
-    for (int i = 0; i < floatVector.size(); i++)
-    {
-        std::cout << floatVector[i] << std::endl;
-    }
-
-    std::vector<int> intVector = { 1, 2, 3, GetFive() };
-    for (int i = 0; i < intVector.size(); i++)
-    {
-        std::cout << intVector[i] << std::endl;
-    }
-
-    int intArray[] = { 1, 2, 3, GetFive() };
-    for (int i = 0; i < 4; i++)
-    {
-        std::cout << intArray[i] << std::endl;
-    }
-
-    //int badArray[getFive()];
-
-    std::vector<int> initializedVector(3, 10);
-    for (int i = 0; i < initializedVector.size(); i++)
-    {
-        std::cout << initializedVector[i] << std::endl;
-    }
-
-    std::vector<float> accounts(4, 100.00);
-
-    accounts.resize(8, 200.00);
-
-    for (int i = 0; i < accounts.size(); i++)
-    {
-        std::cout << accounts[i] << std::endl;
-    }
-
-    // Both of these lines throw an exception.
-    //std::cout << accounts.at(10) << std::endl;
-    //std::cout << accounts[10] << std::endl;
+    // Accessing out of bounds with both square brackets and at(). They both throw exceptions.
+    std::vector<int> vectorToAccessOutOfBounds = { 22, 23, 24 };
+    //std::cout << vectorToAccessOutOfBounds[10] << std::endl;
+    //std::cout << vectorToAccessOutOfBounds.at(10) << std::endl;
 
     return 0;
 }
