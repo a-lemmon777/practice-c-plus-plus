28b0f52be41da4752c0bad00bd53a791b612b350
Aaron Lemmon <a.lemmon777@gmail.com>
2023-04-15 11:29:41 -0500

    Added many more examples of using a deque.

--- 023Deques/src/main.cpp
+++ 023Deques/src/main.cpp
@@ -14,8 +14,7 @@ int main()
     // Checking the size.
     std::cout << "Number of heroes: " << heroes.size() << std::endl;
 
-    // You can access and modify from the front, back, or anywhere in the middle.
-    // Access to the front and back takes constant time.
+    // You can access and modify from the front, back, or anywhere in the middle, all in constant time.
     heroes.front().append("est");
     heroes[1].append("e");
     heroes.at(1).append("st");
@@ -35,7 +34,7 @@ int main()
     heroes.push_back("Enpira the Tall");
 
     // Looping with an iterator.
-    for (std::deque<std::string>::iterator iter = heroes.begin(); iter != heroes.end(); iter++)
+    for (std::deque<std::string>::iterator iter = heroes.begin(); iter != heroes.end(); ++iter)
     {
         std::cout << *iter << std::endl;
     }
@@ -46,7 +45,7 @@ int main()
     heroes.pop_back();
 
     // Using an index-based loop.
-    for (int i = 0; i < heroes.size(); i++)
+    for (int i = 0; i < heroes.size(); ++i)
     {
         std::cout << heroes[i] << std::endl;
     }
@@ -54,6 +53,57 @@ int main()
 
     // Check if the deque is empty.
     std::cout << "Are all the hereos gone?: " << heroes.empty() << std::endl;
+
+    // Insert using an iterator. Inserting into the interior of a deque takes O(n) time.
+    std::deque<std::string>::const_iterator iterator = heroes.cbegin();
+    iterator += 2; // Move the iterator to index 2.
+    heroes.insert(iterator, "Thefrish the Mighty");
+
+    // Using a reverse iterator to traverse the deque in reverse.
+    std::cout << "Printing in reverse." << std::endl;
+    for (std::deque<std::string>::const_reverse_iterator iter = heroes.crbegin(); iter != heroes.crend(); ++iter)
+    {
+        std::cout << *iter << std::endl;
+    }
+    std::cout << "________" << std::endl;
+
+    // Erase using an iterator. Erasing from the interior of a deque takes O(n) time.
+    iterator = heroes.cbegin() + 4; // Move the iterator to index 4.
+    heroes.erase(iterator);
+
+    for (auto iter = heroes.cbegin(); iter != heroes.cend(); ++iter)
+    {
+        std::cout << *iter << std::endl;
+    }
+    std::cout << "________" << std::endl;
+
+    // Using assign to copy values from another container to a deque. This replaces any contents that were already there.
+    sameHeroes.assign(heroes.cbegin(), heroes.cend());
+    for (std::string hero : sameHeroes)
+    {
+        std::cout << hero << std::endl;
+    }
+    std::cout << "________" << std::endl;
+
+    // You can also use assign for placing many copies of a single value into the deque.
+    sameHeroes.assign(6, "Rupligator the Duplicator");
+    for (std::string hero : sameHeroes)
+    {
+        std::cout << hero << std::endl;
+    }
+    std::cout << "________" << std::endl;
+
+    // You can also use assign with an initializer list.
+    sameHeroes.assign({ "Abrizor the Adept", "Belvrica the Bold" });
+    for (std::string hero : sameHeroes)
+    {
+        std::cout << hero << std::endl;
+    }
+    std::cout << "________" << std::endl;
+
+    // You can use clear to empty the deque.
+    sameHeroes.clear();
+    std::cout << "Number of heroes in sameHeroes: " << sameHeroes.size() << std::endl;
 
     return 0;
 }
